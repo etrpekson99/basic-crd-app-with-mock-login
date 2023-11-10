167929d08c57e65228b7d4e77477f2376af7b433
eli <etrpekson1998@gmail.com>
2023-11-11 02:49:46 +0800

    feat: Update configurations

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+const config = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['var(--font-poppins)']
+      }
+    },
+  },
+  plugins: []
+};
+
+module.exports = config;
